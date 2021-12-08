3caaf0b825f50508ff9283646dfd09f00512e89a
Ubuntu <ubuntu@ip-172-31-47-41.ap-northeast-2.compute.internal>
2021-12-08 10:49:59 +0000

    insert and find

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,26 +1,154 @@
 #include "rbtree.h"
 
 #include <stdlib.h>
+void rb_insert_fixup(rbtree* t, node_t* z);
+void left_rotate(rbtree *t, node_t* x);
+void right_rotate(rbtree *t, node_t* x);
+void del_node(rbtree *t,node_t *n);
 
 rbtree *new_rbtree(void) {
+  //rbtree 사이즈 1개의 메모리를 할당하고 모두 0으로 초기화한다.
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
+  //sentinel을 사용하기 때문에 nil 설정해줌
+  node_t* nilnode = (node_t *)calloc(1, sizeof(node_t));
+  nilnode->color = RBTREE_BLACK;
+  p->nil = nilnode;
+  p->root = nilnode;  
   return p;
 }
-
+void del_node(rbtree *t,node_t *n){
+  if(n != t->nil){
+    del_node(t, n->left);
+    del_node(t, n->right);
+    free(n);
+  }
+  
+}
 void delete_rbtree(rbtree *t) {
-  // TODO: reclaim the tree nodes's memory
+  del_node(t,t->root);
+  free(t->nil);
   free(t);
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
-  // TODO: implement insert
-  return t->root;
+  //노드를 새로 만들어줘야 한다
+  //node_t라는 구조체가 이미 있으므로 이 size로 메모리 할당
+  node_t* z = (node_t*)malloc(sizeof(node_t));
+  z->key = key;
+  
+  node_t* y = t->nil; // y를 x의 부모를 따라가는 pointer로 설정
+  node_t* x = t->root; // x를 노드를 따라가는 pointer로 설정, 처음에는 root
+  while(x != t->nil){
+    y = x; //현재 값을 y에 넣어두고 바꾸면 parent가 된다
+    if(z->key < x->key){
+      x = x->left;
+    }else{
+      x = x->right;
+    }
+  }
+  z->parent = y;
+  if (y == t->nil){
+    t->root = z;
+  }else if(z->key < y->key){
+    y->left = z;
+  }else{
+    y->right = z;
+  }
+  z->left = z->right = t->nil;
+  z->color = RBTREE_RED;
+  rb_insert_fixup(t,z);
+  t->root->color = RBTREE_BLACK;
+  return z;
 }
-
+void rb_insert_fixup(rbtree* t, node_t* z){
+  while(z->parent->color == RBTREE_RED){
+    if(z->parent == z->parent->parent->left){
+      node_t* y = z->parent->parent->right;
+      //경우 1
+      if(y->color == RBTREE_RED){
+        z->parent->color = RBTREE_BLACK;
+        y->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        z = z->parent->parent;
+      }else{
+        //경우 2
+        if(z==z->parent->right){
+          z = z->parent;
+          left_rotate(t,z);
+        }
+        //경우 3
+        z->parent->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        right_rotate(t,z->parent->parent);
+      }
+    }else{
+      node_t* y = z->parent->parent->left;
+      if(y->color == RBTREE_RED){
+        z->parent->color = RBTREE_BLACK;
+        y->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        z = z->parent->parent;
+      }else{
+        if(z==z->parent->left){
+          z = z->parent;
+          right_rotate(t,z);
+        }
+        z->parent->color = RBTREE_BLACK;
+        z->parent->parent->color = RBTREE_RED;
+        left_rotate(t,z->parent->parent);
+      }
+    }
+  }
+}
+void left_rotate(rbtree *t, node_t* x){
+  node_t* y = x->right;
+  x->right = y->left;
+  if (y->left != t->nil){
+    y->left->parent = x;
+  }
+  y->parent = x->parent;
+  if (x->parent == t->nil){
+    t->root = y;                      
+  }else if(x == x->parent->left){
+    x->parent->left = y;
+  }else{
+    x->parent->right = y;
+  }
+  y->left = x;
+  x->parent = y;
+} 
+void right_rotate(rbtree *t, node_t* y){
+  node_t*x = y->left;
+  y->left = x->right;
+  if(x->right != t->nil){
+    x->right->parent = y;
+  }
+  x->parent = y->parent;
+  if (y->parent == t->nil){
+    t->root = x;       
+  } else if (y == y->parent->right){
+    y->parent->right = x;
+  }else{
+    y->parent->left = x;
+  }
+  x->right = y;
+  y->parent = x;
+}
 node_t *rbtree_find(const rbtree *t, const key_t key) {
-  // TODO: implement find
-  return t->root;
+  if(t->root == t->nil){
+    return NULL;
+  }
+  node_t* x = t->root; // node_t*형을 변환하는 x를 선언해서 node들을 가리키는 놈으로 사용
+  while (x != t->nil){
+    if (key < x->key){
+      x = x->left;
+    }else if (key > x->key){
+      x = x->right;
+    }else{
+      return x;
+    }
+  }
+  return NULL; // while문을 탈출했다는건 x==t.nil이라는 것이다.
 }
 
 node_t *rbtree_min(const rbtree *t) {
@@ -34,7 +162,7 @@ node_t *rbtree_max(const rbtree *t) {
 }
 
 int rbtree_erase(rbtree *t, node_t *p) {
-  // TODO: implement erase
+  // 밥먹고 여기부터 시작
   return 0;
 }
 
